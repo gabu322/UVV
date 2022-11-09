39e85310a69cbd339d8d1205321f167809c4f2f1
Gabriel DallOrto <gabrieldallorto@hotmail.com>
2022-11-09 03:13:36 -0300

    SQL Power Architect and all comments os PostgreSQL done

--- semestre1/bancoDeDados/pset1/cc1n_202204871_postgresql_hr.sql
+++ semestre1/bancoDeDados/pset1/cc1n_202204871_postgresql_hr.sql
@@ -1,43 +1,53 @@
+DROP TABLE IF EXISTS empregados, regioes, paises, departamentos, localizacoes, cargos, historico_cargos;
+
+/*Inicio da criaçao de tabelas*/
+
+/*Criação da tabela empregados com restrições básicas*/
 CREATE TABLE empregados(
     id_empregado INTEGER NOT NULL PRIMARY KEY,
     nome VARCHAR(75) NOT NULL,
     email VARCHAR(30) NOT NULL UNIQUE,
     telefone VARCHAR(20) NOT NULL,
     data_contratacao DATE NOT NULL,
-    id_cargo VARCHAR(10) NOT NULL,
+    id_cargo VARCHAR(10) NOT NULL, /*FK cargos*/
     salario DECIMAL(8, 2),
     comissao DECIMAL(4, 2),
-    id_departamento INTEGER,
-    id_supervisor INTEGER
+    id_departamento INTEGER, /*FK departamentos*/
+    id_supervisor INTEGER /*FK empregados*/
 );
 
+/*Criação da tabela regioes com restrições básicas*/
 CREATE TABLE regioes(
     id_regiao INTEGER NOT NULL PRIMARY KEY,
     nome VARCHAR(25) NOT NULL UNIQUE
 );
 
+/*Criação da tabela paises com restrições básicas*/
 CREATE TABLE paises(
     id_pais CHAR(2) NOT NULL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL UNIQUE,
-    id_regiao INTEGER 
+    id_regiao INTEGER /*FK regioes*/
 );
 
+/*Criação da tabela localizacoes com restrições básicas*/
 CREATE TABLE localizacoes(
     id_localizacao INTEGER NOT NULL PRIMARY KEY,
     endereco VARCHAR(50),
     cep VARCHAR(12),
     cidade VARCHAR(50),
     uf VARCHAR(25),
-    id_pais CHAR(2)
+    id_pais CHAR(2) /*FK paises*/
 );
 
+/*Criação da tabela departamentos com restrições básicas*/
 CREATE TABLE departamentos(
     id_departamento INTEGER NOT NULL PRIMARY KEY,
     nome VARCHAR(50) UNIQUE,
-    id_localizacao INTEGER,
-    id_gerente INTEGER
+    id_localizacao INTEGER, /*FK localizacoes*/
+    id_gerente INTEGER /*FK empregados*/
 );
 
+/*Criação da tabela cargos com restrições básicas*/
 CREATE TABLE cargos(
     id_cargo VARCHAR(10) NOT NULL PRIMARY KEY,
     cargo VARCHAR(35) NOT NULL UNIQUE,
@@ -45,36 +55,110 @@ CREATE TABLE cargos(
     salario_maximo DECIMAL(8, 2)
 );
 
+/*Criação da tabela historico_cargos com restrições básicas*/
 CREATE TABLE historico_cargos(
-    id_empregado INTEGER NOT NULL, 
+    id_empregado INTEGER NOT NULL, /*FK empregados*/
     data_inicial DATE NOT NULL,
     data_final DATE NOT NULL,
-    id_cargo VARCHAR NOT NULL,
-    id_departamento INTEGER,
+    id_cargo VARCHAR NOT NULL, /*FK cargos*/
+    id_departamento INTEGER, /*FK departamentos*/
     PRIMARY KEY (id_empregado,data_inicial)
 );
 
+
+/*Início da criação das foreign keys*/
+
+/*FKs da tabela empregados*/
+/*FK de empregados em cargos da coluna 'id_cargo'*/
 ALTER TABLE empregados
     ADD FOREIGN KEY (id_cargo) REFERENCES cargos(id_cargo);
+/*FK de empregados em departamentos da coluna 'id_departamento'*/
 ALTER TABLE empregados
     ADD FOREIGN KEY (id_departamento) REFERENCES departamentos(id_departamento);
+/*FK de empregados em empregados da coluna 'id_supervisor' em 'empregado'*/
 ALTER TABLE empregados
     ADD FOREIGN KEY (id_supervisor) REFERENCES empregados(id_empregado);
     
-
+/*FKs da tabela paises*/
+/*FK de paises em regioes da coluna 'id_regiao'*/
 ALTER TABLE paises
     ADD FOREIGN KEY (id_regiao) REFERENCES regioes(id_regiao);
 
+/*FKs da tabela localizacoes*/
+/*FK de localizacoes em paises da coluna 'id_pais'*/
 ALTER TABLE localizacoes
     ADD FOREIGN KEY (id_pais) REFERENCES paises(id_pais);
 
+/*FKs da tabela departamentos*/
+/*FK de departamentos em localizacoes da coluna 'id_localizacao'*/
 ALTER TABLE departamentos
     ADD FOREIGN KEY (id_localizacao) REFERENCES localizacoes(id_localizacao);
+/*FK de departamentos em empregados da coluna 'id_gerente' em 'id_empregado'*/
 ALTER TABLE departamentos
     ADD FOREIGN KEY (id_gerente) REFERENCES empregados(id_empregado);
-    
+
+/*FKs da tabela historico_cargos*/
+/*FK de historico_cargos em cargos da coluna 'id_cargo'*/
 ALTER TABLE historico_cargos
     ADD FOREIGN KEY (id_cargo) REFERENCES cargos(id_cargo);
+/*FK de historico_cargos em departamentos da coluna 'id_departamento'*/
 ALTER TABLE historico_cargos
     ADD FOREIGN KEY (id_departamento) REFERENCES departamentos(id_departamento);
+/*FK de historico_cargos em empregados da coluna 'id_empregado'*/
+ALTER TABLE historico_cargos
+    ADD FOREIGN KEY (id_empregado) REFERENCES empregados(id_empregado);
 
+
+/*Início da adição de comentários*/
+/*Comentários da tabela empregados:*/
+COMMENT ON TABLE empregados IS 'Tabela que contém todas as informações dos empregados.';
+COMMENT ON COLUMN empregados.id_empregado IS 'Chave primária de identificação dos empregados.';
+COMMENT ON COLUMN empregados.nome IS 'Nome do empregado.';
+COMMENT ON COLUMN empregados.email IS 'Endereço de email do empregado.';
+COMMENT ON COLUMN empregados.telefone IS 'Telefone do empregado.';
+COMMENT ON COLUMN empregados.data_contratacao IS 'Data de contratação do empregado.';
+COMMENT ON COLUMN empregados.id_cargo IS 'Chave estrangeira que indica o cargo atual do empregado.';
+COMMENT ON COLUMN empregados.salario IS 'Salário mensal do empregado.';
+COMMENT ON COLUMN empregados.comissao IS 'Comissão em porcentagem do empregado (nem todos os setores possuem comissão).';
+COMMENT ON COLUMN empregados.id_departamento IS 'Chave estrangeira que indica o departamento do empregado.';
+COMMENT ON COLUMN empregados.id_supervisor IS 'Chave estrangeira para a própria tabela que indica o supervisor (que também é um empregado).';
+
+/*Comentários da tabela regioes*/
+COMMENT ON TABLE regioes IS 'Tabela que contém todas as informações das regiões.';
+COMMENT ON COLUMN regioes.id_regiao IS 'Chave primária de identificação das regiões.';
+COMMENT ON COLUMN regioes.nome IS 'Nome das regiões.';
+
+/*Comentários da tabela paises*/
+COMMENT ON TABLE paises IS 'Tabela que contém as informações sobre os países.';
+COMMENT ON COLUMN paises.id_pais IS 'Chave primária de identificação dos países.';
+COMMENT ON COLUMN paises.nome IS 'Chave única para indicar o nome do país.';
+COMMENT ON COLUMN paises.id_regiao IS 'Chave primária de identificação das regiões.';
+
+/*Comentários da tabela localizacoes*/
+COMMENT ON TABLE localizacoes IS 'Tabela que contém as informações sobre as localizações dos departamentos.';
+COMMENT ON COLUMN localizacoes.endereco IS 'Coluna que indica o endereço(logadouro e número) do local da empresa.';
+COMMENT ON COLUMN localizacoes.cep IS 'CEP do local da empresa.';
+COMMENT ON COLUMN localizacoes.cidade IS 'Cidade do local da empresa.';
+COMMENT ON COLUMN localizacoes.uf IS 'Estado (unidade federal) do local da empresa.';
+COMMENT ON COLUMN localizacoes.id_pais IS 'Chave estrangeira que indica o país que está o local da empresa.';
+
+/*Comentários da tabela departamentos*/
+COMMENT ON TABLE departamentos IS 'Tabela que contém as informações de cada departamento da empresa.';
+COMMENT ON COLUMN departamentos.id_departamento IS 'Chave primária de identificação dos departamentos.';
+COMMENT ON COLUMN departamentos.nome IS 'Nome do departamento.';
+COMMENT ON COLUMN departamentos.id_gerente IS 'Chave estrangeira para a identificação do gerente do departamento.';
+
+/*Comentários da tabela cargos*/
+COMMENT ON TABLE cargos IS 'Tabela que contém as informações dos cargos (salário mínimo e máximo).';
+COMMENT ON COLUMN cargos.id_cargo IS 'Chave primária de identificação dos cargos.';
+COMMENT ON COLUMN cargos.cargo IS 'Nome do cargo.';
+COMMENT ON COLUMN cargos.salario_minimo IS 'Salário mínimo do cargo.';
+COMMENT ON COLUMN cargos.salario_maximo IS 'Salário máximo do cargo.';
+
+/*Comentários da tabela historico_cargos*/
+COMMENT ON TABLE historico_cargos IS 'Tabela que contém a informação sobre o histórico de cargos dos empregados entre os departamentos.';
+COMMENT ON COLUMN historico_cargos.id_empregado IS 'Chave primária estrangeira que indica o histórico do cargo dos empregados.';
+COMMENT ON COLUMN historico_cargos.data_inicial IS 'Chave primária que indica a data de início do cargo.';
+COMMENT ON COLUMN historico_cargos.data_final IS 'Data de finalização do cargo.';
+COMMENT ON COLUMN historico_cargos.id_cargo IS 'Chave estrangeira que indica o cargo que foi alterado.';
+COMMENT ON COLUMN historico_cargos.id_departamento_1 IS 'Chave estrangeira para a identificação do departamento.';
--- semestre1/bancoDeDados/pset1/perguntas.md
+++ semestre1/bancoDeDados/pset1/perguntas.md
@@ -1,9 +1,61 @@
 # Respostas para perguntas do pset1
 
 1. O que são sistemas de controles de versões? Por que são importantes?  
-    Resposta: Eles são, como o nome já diz, sistemas que permitem o controle de versões de um arquvo, permitindo a vizualização e gerenciamento de versões anteriores. Eles são fundamentais para garantir a possibilidade de voltar a versões anteriores e descobrir bugs.
+    **Resposta:** Eles são, como o nome já diz, sistemas que permitem o controle de versões de um arquvo, permitindo a vizualização e gerenciamento de versões anteriores. Eles são fundamentais para garantir a possibilidade de voltar a versões anteriores e descobrir bugs.
+
 2. Qual a diferença entre o Git e o GitHub? Como eles estão relacionados?
 É possível usar um sem o outro?  
-    Resposta: Git é o software/sistema de controle de versão, e o GitHub é um site onde você pode compartilhar todas as versões do seu programa que foram salvas utilizando o Git, e também deixar todas as versões salvas lá. É possível utilizar o Git sem o Github, pois ele fica apenas no seu computador, mas não é possível utilizar o Github sem o Git, já que ele depende das versões salvas pelo git para funcionar.
+    **Resposta:** Git é o software/sistema de controle de versão, e o GitHub é um site onde você pode compartilhar todas as versões do seu programa que foram salvas utilizando o Git, e também deixar todas as versões salvas lá. É possível utilizar o Git sem o Github, pois ele fica apenas no seu computador, mas não é possível utilizar o Github sem o Git, já que ele depende das versões salvas pelo git para funcionar.
+
 3. O Git é um sistema distribuído de controle de versões. O que significa isso?  
-    Resposta: Isso significa que no git cada usuários possui um repositório próprio e específico pra cada um, onde no sistema cental existe um repositório central para todos os usuários.
+**Resposta:** Isso significa que no git cada usuários possui um repositório próprio e específico pra cada um, onde no sistema cental existe um repositório central para todos os usuários
+
+4. Existem alguns errinhos no projeto HR ilustrado acima (principalmente
+em relação às cardinalidades dos relacionamentos e em relação à obrigatoriedade das
+chaves estrangeiras). Identifique quais são esses erros, explique o que está errado e
+conserte os erros se for necessário.  
+**Resposta:** '#'
+
+5. Alguma tabela do projeto representa um relacionamento do tipo N:N?
+Se sim, identifique a tabela e explique porque é um relacionamento N:N; Se não,
+explique porque não há relacionamentos N:N neste projeto.  
+**Resposta:** Sim, na tabela historico_cargos. A relação N:N acontece entre as tabelas departamentos e empregados, onde um empregado pode mudar de departamento dentro de um cargo, ou mudar de cargo dentro de um departamento.
+
+6. Pelo projeto apresentado é possível que um determinado empregado
+seja o gerente de zero, um ou mais departamentos. Se eu quisesse impor uma regra
+que diz que um empregado só pode ser gerente de, no máximo, um departamento,
+o que eu teria que fazer no projeto? O que eu teria que criar no banco de dados para
+impor essa restrição? Por quê?  
+**Resposta:** Seria necessário fazer com que a coluna 'id_gerente' da tabela 'departamentos' se torne uma primary key ou uma chave alternativa (única), por que assim seria necessário que um departamento tivesse gerente (por primary keys não poderem ser NULL), e teria apenas 1 gerente por departamento (por primary keys serem únicas).
+
+7. Por que os relacionamentos entre as tabelas estão representados com
+linhas pontilhadas? O que isso representa? Por que foi representado assim?  
+**Resposta:**
+
+8. Qual é o único tipo de relacionamento que pode guardar dados? Por
+quê? Existe algum relacionamento assim nesse projeto? Se não existir, você sugeriria trocar alguns dos relacionamentos existentes para melhorar o projeto?  
+**Resposta>**
+
+9. Explique o relacionamento da tabela empregados com ela mesma? É
+um erro? É correto? Por quê?  
+**Resposta:** Este relacionamento está correto. Ele serve para indicar que uma pessoa, que possui os dados na mesma tabela como todas as outras, é o valor a ser preenchido na coluna, ou seja, uma pessoa na tabela empregados possui um gerente que também está na tabela empregados.
+
+10. Qual a diferença entre banco de dados, usuário e esquema no PostgreSQL?  
+**Resposta:** Banco de dados é a parte geral do projeto, onde contém todos os arquivos, esquemas e informações das tabelas. Usuário é a "conta" em que será digitado e requisitado os códigos e informações. Esquema é o lugar onde ficam armazenados as tabelas, linhas, informações, 'constraints', e etc, basicamente todas as informações que envolvem tabelas do banco de dados.
+
+11. Por que um esquema é importante?  
+**Resposta** Ele é importante para permitir que vários usuários possam utilizar o banco de dados sem que uma ação de um usuário interfira com outro.
+12. Se você não definir um esquema específico, onde os objetos do banco
+de dados (tabelas, relacionamentos, dados, etc.) serão gravados? Isso é bom ou
+ruim? Por que?  
+**Resposta** Eles serão gravados no esquema público. Isso é ruim pois esse é o esquema padrão e geral, portanto alguma alteração nesse esquema irá afetar todos os outros usuários que estejam conectado nele, portanto o ideal é que cada um utilize em situações normais o próprio esquema.
+13. Agora que você já implementou o projeto no PostgreSQL, tem alguma
+sugestão de melhoria a fazer para o projeto? Como ele poderia ser melhorado?  
+**Resposta:** Algumas possíveis melhoras para ele seria a remoção da tabela regiões e talvez até a remoção da tabela países, pois as informações contidas nessas tabelas poderiam facilmente serem incluidas direto na tabela localizações.
+
+14. Faça uma comparação dos SGBD que você utilizou. Quais as vantagnes e desvantagens de cada um? Quem tem a melhor documentação?  
+**Resposta:**
+15. Como você acha que foi seu desempenho neste PSet? Como você foi
+buscando as informações necessárias e lendo as documentações dos SGBD? Você
+aproveitou a oportunidade e buscou ajuda dos monitores?  
+**Resposta**
